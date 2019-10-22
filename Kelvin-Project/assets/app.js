410616ef9be2cce28e5f2cc786674f7d5bbf2ec2
Senenter <54664461+Senenter@users.noreply.github.com>
2019-10-22 15:38:48 +0200

    Update app.js

--- Kelvin-Project/assets/app.js
+++ Kelvin-Project/assets/app.js
@@ -1,2 +1,9 @@
 //This variable, a const is named kelvin and is set to 293
 const kelvin = 293;
+//the const celsius is assigned the number resulting from the calculation
+const celsius = kelvin - 273;
+//The let fahrenheit is assigned the number resulting from the calculation but can be changed since it is a let variable
+let fahrenheit = celsius * (9/5) + 32;
+//Rounds down the number from fahrenheit if it is a decimal number
+fahrenheit = Math.floor(fahrenheit);
+console.log(`The temprature is ${fahrenheit} degrees Fahrenheit.`);
