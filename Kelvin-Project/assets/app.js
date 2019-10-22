8cd07d255a0ca5e178bc549b78077fda06dd9a0b
Senenter <54664461+Senenter@users.noreply.github.com>
2019-10-22 15:44:49 +0200

    dog years

--- Kelvin-Project/assets/app.js
+++ Kelvin-Project/assets/app.js
@@ -7,3 +7,9 @@ let fahrenheit = celsius * (9/5) + 32;
 //Rounds down the number from fahrenheit if it is a decimal number
 fahrenheit = Math.floor(fahrenheit);
 console.log(`The temprature is ${fahrenheit} degrees Fahrenheit.`);
+//---------------------------------------------------------------------------------
+// const kelvin = 293;
+// const celsius = kelvin - 273;
+// let newton = celsius * (33/100);
+// fahrenheit = Math.floor(newton);
+// console.log(`The temprature is ${newton} degrees Newton.`);
