da8d2e7968ec8d35f24e42a21048deed4e242cea
Senenter <54664461+Senenter@users.noreply.github.com>
2019-10-25 16:13:11 +0200

    Update app.js

--- Conditionals/assets/app.js
+++ Conditionals/assets/app.js
@@ -1,4 +1,4 @@
-//Les 1 - The if keyword
+//Les 2 - The if keyword
 
 // let sale = true;
 // sale = false;
@@ -6,7 +6,7 @@
 //   console.log('Time to buy!');
 // }
 //--------------------------------------------------------------------------------
-//Les 2 - if...Else Statements
+//Les 3 - if...Else Statements
 
 let sale = true;
 sale = false;
@@ -14,4 +14,22 @@ if(sale){
   console.log('Time to buy!');
 } else{
   console.log('Time to wait for a sale.');
+}
+//--------------------------------------------------------------------------------
+//Les 4 - Comparison Operators
+
+/*
+Less than: <
+Greater than: >
+Less than or equal to: <=
+Greater than or equal to: >=
+Is equal to: ===
+Is NOT equal to: !==
+*/
+
+let hungerLevel = 9;
+if (hungerLevel > 7){
+  console.log('Time to eat!');
+}else{
+  console.log('We can eat later!');
 }
