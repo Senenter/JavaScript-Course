6da2bee39885c45106804be9e8e495dbe7d7f3ce
Senenter <54664461+Senenter@users.noreply.github.com>
2019-10-25 16:05:56 +0200

    Update app.js

--- Conditionals/assets/app.js
+++ Conditionals/assets/app.js
@@ -1,5 +1,17 @@
+//Les 1 - The if keyword
+
+// let sale = true;
+// sale = false;
+// if(sale){
+//   console.log('Time to buy!');
+// }
+//--------------------------------------------------------------------------------
+//Les 2 - if...Else Statements
+
 let sale = true;
 sale = false;
 if(sale){
   console.log('Time to buy!');
+} else{
+  console.log('Time to wait for a sale.');
 }
