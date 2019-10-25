99eec3b2ed0bd967eb56861ea77cef968cc4ad74
Senenter <54664461+Senenter@users.noreply.github.com>
2019-10-25 11:32:00 +0200

    Update app.js

---
+++ Dog Years-Project/assets/app.js
@@ -0,0 +1,7 @@
+//A variable that defines human age. In this case it is a const variable since in this code it will not be changed
+const myAge = 24;
+//A variable that defines the early years of a dogs life. It is a let variable since it will be changed
+let earlyYears = 2;
+
+earlyYears = earlyYears * 10.5
+console.log(earlyYears);
