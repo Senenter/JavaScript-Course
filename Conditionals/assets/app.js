bf832a0bb93885266f28c099cc84617be0cd2c1a
Senenter <54664461+Senenter@users.noreply.github.com>
2019-10-27 13:51:09 +0100

    Update app.js

--- Conditionals/assets/app.js
+++ Conditionals/assets/app.js
@@ -33,3 +33,55 @@ if (hungerLevel > 7){
 }else{
   console.log('We can eat later!');
 }
+ //--------------------------------------------------------------------------------
+ //Les 5 - Logical Operators
+
+let mood = 'sleepy';
+let tirednessLevel = 8;
+if( mood === 'sleepy' && tirednessLevel > 8){
+  console.log('time to sleep');
+}else{
+  console.log('not bed time yet');
+}
+ //--------------------------------------------------------------------------------
+ //Les 6 - Truthy and Falsy
+
+ let wordCount = 0;
+
+if (wordCount) {
+  console.log("Great! You've started your work!");
+} else {
+  console.log('Better get to work!');
+}
+
+
+let favoritePhrase = 'Hello World!';
+
+if (favoritePhrase) {
+  console.log("This string doesn't seem to be empty.");
+} else {
+  console.log('This string is definitely empty.');
+}
+//--------------------------------------------------------------------------------
+//Les 7 - Truthy and Falsy Assignment
+
+let tool = 'marker';
+
+// Use short circuit evaluation to assign  writingUtensil variable below:
+let writingUtensil = tool || 'pen';
+
+console.log(`The ${writingUtensil} is mightier than the sword.`);
+//--------------------------------------------------------------------------------
+//Les 8 - Ternary Operator
+
+let isLocked = false;
+
+isLocked ? console.log('You will need a key to open the door.') : console.log('You will not need a key to open the door.');
+
+let isCorrect = true;
+
+isCorrect ? console.log('Correct!') : console.log('Incorrect!');
+
+let favoritePhrase = 'Love That!';
+
+favoritePhrase === 'Love That!' ? console.log('I love that!') : console.log("I don't love that!");
