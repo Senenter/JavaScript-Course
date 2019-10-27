d2ad621a07fe09f0c03baeba5e35ea6ede74dd60
Senenter <54664461+Senenter@users.noreply.github.com>
2019-10-27 14:17:19 +0100

    Update app.js

--- Conditionals/assets/app.js
+++ Conditionals/assets/app.js
@@ -103,3 +103,21 @@ if (season === 'spring') {
 }
 //--------------------------------------------------------------------------------
 //Les 10 - The switch keyword
+
+let athleteFinalPosition = 'first place';
+
+switch (athleteFinalPosition) {
+  case 'first place':
+    console.log('You get the gold medal!');
+    break;
+  case 'second place':
+    console.log('You get the silver medal!');
+    break;
+  case 'third place':
+  console.log('You get the bronze medal!');
+    break;
+  default:
+  console.log('No medal awarded.');
+    break;
+
+}
