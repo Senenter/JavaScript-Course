fc59b5801724f9273b49083a9c21141f39184437
Senenter <54664461+Senenter@users.noreply.github.com>
2019-10-28 19:52:25 +0100

    Les 4

--- Functions/assets/app.js
+++ Functions/assets/app.js
@@ -7,3 +7,13 @@ function getReminder(){
 function greetInSpanish(){
   console.log('Buenas Tardes.');
 }
+//------------------------------------------------------------------------------
+//Les 3 - Calling a Function
+
+function sayThanks(){
+  console.log('Thank you for your purchase! We appreciate your business.');
+}
+
+sayThanks();
+sayThanks();
+sayThanks();
