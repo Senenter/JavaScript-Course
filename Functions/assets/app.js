5a0a1a40a1125341f9ed7989b5d09c6a491f5463
Senenter <54664461+Senenter@users.noreply.github.com>
2019-10-28 20:43:47 +0100

    Les 10

--- Functions/assets/app.js
+++ Functions/assets/app.js
@@ -1,19 +1,86 @@
 //Les 2 - Function Declarations
 
-function getReminder(){
+function getReminder() {
   console.log('Water the plants.');
 }
 
-function greetInSpanish(){
+function greetInSpanish() {
   console.log('Buenas Tardes.');
 }
 //------------------------------------------------------------------------------
 //Les 3 - Calling a Function
 
-function sayThanks(){
+function sayThanks() {
   console.log('Thank you for your purchase! We appreciate your business.');
 }
 
 sayThanks();
 sayThanks();
 sayThanks();
+//------------------------------------------------------------------------------
+//Les 4 - Parameters and Arguments
+
+function sayThanks(name) {
+  console.log('Thank you for your purchase ' + name + '! We appreciate your business.');
+}
+
+sayThanks('Cole');
+//------------------------------------------------------------------------------
+//Les 5 - Default Parameters
+
+function makeShoppingList(item1 = 'milk', item2 = 'bread', item3 = 'eggs') {
+  console.log(`Remember to buy ${item1}`);
+  console.log(`Remember to buy ${item2}`);
+  console.log(`Remember to buy ${item3}`);
+}
+//------------------------------------------------------------------------------
+//Les 6 - Return
+
+function monitorCount(rows, columns) {
+  return rows * columns;
+
+}
+const numOfMonitors = monitorCount(5, 4);
+console.log(numOfMonitors);
+//------------------------------------------------------------------------------
+//Les 7 - Helper Functions
+
+function monitorCount(rows, columns) {
+  return rows * columns;
+}
+
+function costOfMonitors(rows, columns) {
+  return monitorCount(rows, columns) * 200
+}
+
+const totalCost = costOfMonitors(5, 4);
+
+console.log(totalCost);
+//------------------------------------------------------------------------------
+//Les 8 - Function Expressions
+
+const plantNeedsWater = function(day) {
+  if (day === 'Wednesday') {
+    return true;
+  } else {
+    return false;
+  }
+};
+plantNeedsWater('Tuesday')
+console.log(plantNeedsWater('Tuesday'));
+//------------------------------------------------------------------------------
+//Les 9 - Arrow Functions
+
+const plantNeedsWater = (day) => {
+  if (day === 'Wednesday') {
+    return true;
+  } else {
+    return false;
+  }
+};
+//------------------------------------------------------------------------------
+//Les 10 - Concise Body Arrow Functions
+
+const plantNeedsWater = day => {
+  'Wednesday' ? true : false;
+};
