9a0eaf9c001eba39de359e69bfc7bb17e45f26ec
Senenter <54664461+Senenter@users.noreply.github.com>
2019-11-01 23:46:01 +0100

    get computer choice

--- Rock, Paper, or Scissors-Project/assets/app.js
+++ Rock, Paper, or Scissors-Project/assets/app.js
@@ -5,12 +5,31 @@
  */
 const getUserChoice = (userInput) => {
   userInput = userInput.toLowerCase();
-  if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors'){
+  if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors') {
     return userInput;
-  }else{
-    console.log('Error. Please use on of the following: Rock, Paper or Scissors');
+  } else {
+    console.log('Error. Please use one of the following: Rock, Paper or Scissors');
   }
 };
 
-console.log(getUserChoice('Paper'));
-console.log(getUserChoice('Spoon'));
+//console.log(getUserChoice('Paper'));//Logs a valid value
+//console.log(getUserChoice('Spoon'));//Logs an invalid value and gives an error message + undefined since spoon is not defined
+
+/**
+ * Function (made with the fat arror function syntax) for getting the computers choice
+ * Const randomNumber is used with math.floor and math.random to get a random number between 0 and 2
+ */
+
+const getComputerChoice  = () => {
+  const randomNumber = Math.floor(Math.Random() * 3);
+  switch (randomNumber) {
+    case 0:
+      return 'Rock';
+    case 1:
+      return 'paper';
+    case 2:
+      return 'scissors';
+  }
+}
+
+console.log(getComputerChoice());
