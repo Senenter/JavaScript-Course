558d48f2b7bbb848ab155de6cf0036319fc2d7b6
Senenter <jage0030@hz.nl>
2019-11-04 23:23:31 +0100

    Re-structure

---
+++ Functions/Sleep Debt Calculator-Project/assets/app.js
@@ -0,0 +1,20 @@
+const getSleepHours = (day) => {
+  switch (day) {
+    case 'monday':
+      return 8;
+      break;
+    case 'tuesday';
+      return 7;
+      break;
+    case 'wednesday';
+      return 8;
+      break;
+    case 'thursday';
+      return 8;
+      break;
+    case
+    default:
+    return 'Error!'
+
+  }
+};
