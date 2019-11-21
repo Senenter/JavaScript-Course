2f46760c034b83f1d0bbfcf9ddf126a93f0200fd
Senenter <54664461+Senenter@users.noreply.github.com>
2019-11-21 14:25:01 +0100

    Update app.js

---
+++ Arrays/assets/app.js
@@ -0,0 +1,156 @@
+// Lesson 2 - Create an Array
+
+/**
+ * Creates an array containing 3 hobbies, data in an array does not have to be of the same type,
+ * When logging the variable containing the array, we log the contents of the array
+ */
+const hobbies = [
+    'Gaming',
+    'Coding',
+    'Music'
+
+];
+
+console.log(hobbies);
+//-------------------------------------------------------------------------------
+//Lesson 3 - Accesing Elements
+
+const famousSayings = [
+    'Fortune favors the brave.', 
+    'A joke is a very serious thing.', 
+    'Where there is love there is life.'
+
+];
+
+/**
+ *Saves the first item in the array to an variable called listItem
+ */
+const listItem = famousSayings[0];
+
+/**
+ * Logs the last item in the element
+ * The second log gives an undefined becuase there is not a fourth item in the array, there are three
+ */
+
+console.log(famousSayings[2]);
+
+console.log(famousSayings[3]);
+//-------------------------------------------------------------------------------
+// Lesson 4 - Updatre elements
+
+let groceryLists = [
+    'bread', 
+    'tomatoes', 
+    'milk'
+
+];
+
+/**
+ * Updates the array, item [1] is changed from tomatoes to avocados
+ */
+
+groceryLists[1] = 'avocados';
+
+/**
+ * When we log the array, we now get the array containing avocaods instead of tomatoes
+ */
+
+console.log(groceryLists);
+//-------------------------------------------------------------------------------
+//Lesson 5 - Arrays with let and const
+
+let condiments = ['Ketchup', 'Mustard', 'Soy Sauce', 'Sriracha'];
+
+const utensils = ['Fork', 'Knife', 'Chopsticks', 'Spork'];
+/**
+ * Reassigns Mayo to the index location where Ketchup used to be
+ */
+condiments[0] = 'Mayo';
+console.log(condiments);
+/**
+ * Assigns a new array to condiments
+ */
+condiments = ['Mayo']
+console.log(condiments);
+/**
+ * Reassigns Spoon to the index location where spork used to be
+ */
+utensils[3] = 'Spoon'
+console.log(utensils);
+//-------------------------------------------------------------------------------
+//Lesson 6 - The .length property
+
+const objectives = [
+    'Learn a new languages',
+    'Read 52 books',
+    'Run a marathon'
+    
+    ];
+/**
+ * The .length porperty returns how many elements are in the array that it is used on
+ */
+console.log(objectives.length);
+//-------------------------------------------------------------------------------
+//Lesson 7 - The .push() Method
+
+const chores = [
+    'wash dishes',
+    'do laundry', 
+    'take out trash'
+
+];
+/**
+ * Adds, or "Pushes" two items to the end of the array, changing it.
+ */
+chores.push('Walk with the dog', 'Feed the cat');
+console.log(chores);
+//-------------------------------------------------------------------------------
+//Lesson 8 - The .pop() Method
+
+const choresPop = [
+    'wash dishes', 
+    'do laundry', 
+    'take out trash', 
+    'cook dinner', 
+    'mop floor'
+];
+/**
+ * the .pop() function removes the last item fromt he array, mutating it in the process
+ */
+choresPop.pop();
+console.log(choresPop);
+//-------------------------------------------------------------------------------
+// Lesson 9 - More Array Methods
+
+const groceryList = [
+    'orange juice', 
+    'bananas', 
+    'coffee beans', 
+    'brown rice', 
+    'pasta', 
+    'coconut oil', 
+    'plantains'
+
+];
+
+/**
+ * .shift() removes the first item of the array, mutating it in the process
+ */
+groceryList.shift();
+//console.log(grocerylist);
+
+/**
+ * .unshift() adds a item to the beginning of the array, mutating it in the process
+ */
+groceryList.unshift('popcorn');
+console.log(groceryList);
+/**
+ * .slice() return a cope of a portion of the array and adds it into a new array, the original array will not be changed
+ */
+console.log(groceryList.slice(1,4));
+console.log(groceryList);
+
+const pastaIndex = groceryList.indexOf('pasta');
+console.log(pastaIndex);
+
+
