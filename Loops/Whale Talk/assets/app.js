749559228ee49330b35a754b20a079fe4aad9996
Senenter <54664461+Senenter@users.noreply.github.com>
2019-11-25 15:05:57 +0100

    Update app.js

--- Loops/Whale Talk/assets/app.js
+++ Loops/Whale Talk/assets/app.js
@@ -1,4 +1,4 @@
-let input = 'I love to code, eventough i am not good at it';
+let input = 'i love to code, eventough i am not good at it';
 const vowels = [
     'a',
     'e',
@@ -6,3 +6,36 @@ const vowels = [
     'o',
     'u'
 ];
+let resultArray = [];
+
+for (let i = 0; i < input.length; i++) {
+     //console.log('inputIndex = ' + i);
+    for (let j = 0; j < vowels.length; j++) {
+        //console.log('vowelsIndex = ' + j);
+        if (input[i] === vowels[j]){
+            if (input[i] === 'a'){
+                resultArray.push('aa');
+            }
+            else if (input[i] === 'e'){
+                resultArray.push('ee');
+            }
+            else if (input[i] === 'i'){
+                resultArray.push('ii');
+            }
+            else if (input[i] === 'o'){
+                resultArray.push('oo');
+            }
+            else if (input[i] === 'u'){
+                resultArray.push('uu')
+            }
+            else {
+                resultArray.push(input[i]);
+            }
+
+        }
+    
+    }
+
+};
+
+console.log(resultArray.join('').toUpperCase());
